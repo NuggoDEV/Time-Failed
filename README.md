22c87670fb5bbb8b9d75e58f97620dc56cde98f7
Nuggo <NuggoDEV@gmail.com>
2022-11-15 17:32:11 +1030

    Nearly complete lmao

---
+++ README.md
@@ -0,0 +1,9 @@
+# Timed Failed
+
+Shows the time in the song you failed at in the results screen
+
+## Credits
+
+* [zoller27osu](https://github.com/zoller27osu), [Sc2ad](https://github.com/Sc2ad) and [jakibaki](https://github.com/jakibaki) - [beatsaber-hook](https://github.com/sc2ad/beatsaber-hook)
+* [raftario](https://github.com/raftario)
+* [Lauriethefish](https://github.com/Lauriethefish), [danrouse](https://github.com/danrouse) and [Bobby Shmurner](https://github.com/BobbyShmurner) for [this template](https://github.com/Lauriethefish/quest-mod-template)
---
+++ src/TimeFailedHooks.cpp
@@ -0,0 +1,68 @@
+#include "main.hpp"
+#include "Hooks.hpp"
+
+#include "GlobalNamespace/ResultsViewController.hpp"
+//#include "GlobalNamespace/"
+#include "GlobalNamespace/AudioTimeSyncController.hpp"
+using namespace GlobalNamespace;
+
+using namespace UnityEngine;
+
+#include "questui/shared/BeatSaberUI.hpp"
+using namespace QuestUI;
+
+std::string SecondsToString(float value)
+{
+    int minutes = value / 60;
+    int seconds = value - minutes * 60;
+    std::string minuteStr = std::to_string(minutes);
+    std::string secondStr = std::to_string(seconds);
+
+    if (minutes == 0)
+    {
+        return secondStr + "s";
+    }
+    else
+    {
+        return minuteStr + "m :" + secondStr + "s";
+    }
+}
+
+
+int endTime;
+int timeFailed;
+GameObject *container;
+TMPro::TextMeshProUGUI *timeFailedText;
+
+MAKE_AUTO_HOOK_MATCH(ResultsViewController_DidActivate, &ResultsViewController::DidActivate, void, ResultsViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+    ResultsViewController_DidActivate(self, firstActivation, addedToHierarchy, screenSystemEnabling);
+
+    
+    if (firstActivation)
+    {
+        container = BeatSaberUI::CreateFloatingScreen({0, 0}, {0, 0.9, 4.1}, {0, 0, 0}, 0.0f, false, false);
+        timeFailedText = BeatSaberUI::CreateText(container->get_transform(), "Failed at: " + SecondsToString(timeFailed) + " / " + SecondsToString(endTime));
+        timeFailedText->set_alignment(TMPro::TextAlignmentOptions::Center);
+        timeFailedText->set_fontSize(6.0f);
+        getLogger().info("Created text");
+    }
+    else
+    {
+        container->set_active(true);
+        timeFailedText->set_text("Failed at: " + SecondsToString(timeFailed) + " / " + SecondsToString(endTime));
+    }
+}
+
+MAKE_AUTO_HOOK_MATCH(ResultsViewController_DidDeactivate, &ResultsViewController::DidDeactivate, void, ResultsViewController* self, bool removedFromHierarchy, bool systemScreenDisabling) {
+    ResultsViewController_DidDeactivate(self, removedFromHierarchy, systemScreenDisabling);
+
+    container->set_active(false);
+}
+
+MAKE_AUTO_HOOK_MATCH(AudioTimeSyncController_Update, &AudioTimeSyncController::Update, void, AudioTimeSyncController* self) {
+    AudioTimeSyncController_Update(self);
+
+    endTime = self->get_songEndTime();
+    timeFailed = self->songTime;
+    getLogger().info("Updated time");
+}
